923408ab27f7c09f9364be886b17dff18f288c1e
Tobias Wolter <tobias.wolter@b1-systems.de>
2019-01-08 15:48:38 +0100

    Insert space so Strings is not confused by the boilerplate

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 ## copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 ## license: GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 ## author: Tobias Wolter <tobias.wolter@b1-systems.de>, 2018
+
 # @summary Configures a Jitsi Meet instance.
 # @author Tobias Wolter <tobias.wolter@b1-systems.de>
 # @param authentication What kind of authentication to use.
